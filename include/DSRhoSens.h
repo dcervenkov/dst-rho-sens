32425f7a3b109ef78ca6e06f21109e023770d9d1
Daniel Cervenkov <cervenkov@ipnp.troja.mff.cuni.cz>
2011-07-08 12:59:32 +0900

    Added a function that reads the tree and prints out data

--- include/DSRhoSens.h
+++ include/DSRhoSens.h
@@ -1,3 +1,10 @@
+#include "TApplication.h"
+#include "TFile.h"
+#include "TTree.h"
 #include "TRandom1.h"
 #include "TH1F.h"
-#include "TApplication.h"
+
+#include <stdio.h>
+
+static const int maxParticles = 300;
+static const char fileName[] = "data/3events.root";
--- src/DSRhoSens.cpp
+++ src/DSRhoSens.cpp
@@ -1,15 +1,62 @@
 #include "DSRhoSens.h"
 
+int treer()
+{
+    printf("Opening file %s...\n",fileName);
+    TFile *file = new TFile(fileName);
+    if (file->IsOpen() == kTRUE)
+        printf("File %s opened.\n",fileName);
+    TTree *tree = (TTree*)file->Get("Tree");
+    for(int eventNo = 0; eventNo < tree->GetEntries(); eventNo++)
+    {
+        //This code block extracts the number of particles in the event
+        //so arrays of appropriate size can be created
+        int numParticles;
+        tree->SetBranchStatus("*",0);
+        tree->SetBranchStatus("numParticles",1);
+        tree->SetBranchAddress("numParticles",&numParticles);
+        tree->GetEntry(eventNo);
+
+        int id[numParticles];
+        int idhep[numParticles];
+        int mother[numParticles];
+        int da1[numParticles];
+        int da2[numParticles];
+        double p[numParticles][5];
+        double v[numParticles][4];
+
+        tree->SetBranchStatus("*",1);
+        tree->SetBranchStatus("numParticles",0);
+
+        tree->SetBranchAddress("id",id);
+        tree->SetBranchAddress("idhep",idhep);
+        tree->SetBranchAddress("mother",mother);
+        tree->SetBranchAddress("da1",da1);
+        tree->SetBranchAddress("da2",da2);
+        tree->SetBranchAddress("p",p);
+        tree->SetBranchAddress("v",v);
+
+        tree->GetEntry(eventNo);
+
+        printf("ID\tIDHEP\tMOTHER\tDA1\tDA2\tP1\tP2\tP3\tE\tM\n");
+
+        for (int i = 0; i < numParticles; i++)
+        {
+            printf("%i\t%i\t%i\t%i\t%i\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",id[i],idhep[i],\
+                   mother[i],da1[i],da2[i],p[i][0],p[i][1],p[i][2],p[i][3],p[i][4]);
+        }
+        printf("\n\n\n");
+    }
+
+    file->Close();
+    delete file;
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    TApplication* rootapp = new TApplication("example",&argc,argv);
-    TRandom1* myrand = new TRandom1();
-    TH1F* myhist = new TH1F("stats","",100,0,10);
-    for(int i=0;i<10000;++i)
-    {
-        myhist->Fill(myrand->Gaus(5,1));
-    }
-    myhist->Draw();
-    rootapp->Run();
+    //TApplication* rootapp = new TApplication("DSRhoSens",&argc,argv); //For graphic-output apps only
+    treer();
+    //rootapp->Run(); //For graphic-output apps only
     return 0;
 }
